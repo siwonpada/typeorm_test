4c07c14c87bf02cdf166cc6f2d2786305fe89a4b
siwonpada <psw041115@gmail.com>
2022-07-14 18:22:25 +0900

    test_1

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { Photo } from "./entity/Photo"
 
 AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    const photo = new Photo()
+    photo.name = "Me and Bears"
+    photo.description = "I am near polar bears"
+    photo.filename = "photo-with-bears.jpg"
+    photo.views = 1
+    photo.isPublished = true
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    await AppDataSource.manager.save(photo)
+    console.log("Photo has been saved. Photo id is", photo.id)
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    const savedPhotos = await AppDataSource.manager.find(Photo)
+    console.log("All photos from th DB: ", savedPhotos)
 
 }).catch(error => console.log(error))
